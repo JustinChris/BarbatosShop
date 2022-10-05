85aea39b593de188c3f8bb73421aa31346cd9e33
Justin Christian <justinchristian2016@gmail.com>
2022-10-05 12:36:24 +0700

    update routing for login and home

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
-Route::get("/greet", function () {
+Route::get('/greet', function () {
     return view('greeting', ['name' => 'test']);
+});
+
+Route::get('/login', function () {
+    return view('login');
 });
